93d724656747465d7a37352eedb634a5e1f4fcdd
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-14 10:04:54 +0530

    render the sign up page/render the sign in page

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -1,9 +1,11 @@
+// render the sign up page
 module.exports.signUp = function (req, resp) {
   return resp.render("user_sign_up", {
     title: "Codeial | Sign Up",
   });
 };
 
+// render the sign in page
 module.exports.signIn = function (req, resp) {
   return resp.render("user_sign_in", {
     title: "Codieal | Sign In",
